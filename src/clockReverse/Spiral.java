fb3cc2a77e56f0c97c79bb21e5cffc93748c895c
Budai Dani <budai.d945@gmail.com>
2016-09-20 10:16:36 +0200

    v2

--- src/clockReverse/Spiral.java
+++ src/clockReverse/Spiral.java
@@ -1,10 +1,25 @@
 package clockReverse;
 
 public class Spiral {
-	public int N = 3;
-	int [][] m;
-	
-	
-	
-	
+	public int N;
+
+	public static int[][] createSpiral(int N) {
+		int[][] matrix = new int[N][N];
+
+		if (N < 1) {
+			return matrix;
+		}
+		else {
+			for (int i = 1; i < matrix.length; i++) {
+				for (int j = 0; j < matrix.length; j++) {
+					System.out.println(matrix);
+				}
+			}
+		}
+		return matrix;
+	}
+
+	public static void main(String[] args) {
+		createSpiral(3);
+	}
 }
